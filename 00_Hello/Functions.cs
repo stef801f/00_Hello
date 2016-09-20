951a80c07cf7b76dd3cb55317012758911e33f7f
stef801f <stef801f@edu.eal.dk>
2016-09-20 12:41:09 +0200

    Add project files.

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _00_Hello
+{
+    internal class Functions
+    {
+        internal static string Hello()
+        {
+            return "hello";
+        }
+
+        internal static object Hello(string v)
+        {
+            return "hello, " + v;
+        }
+    }
+}
